8179a9585081dec4564689f8c413b529ece0f596
Coleman B Gentry <104287033+colemanbgentry@users.noreply.github.com>
2022-09-16 11:12:14 -0700

    Missed a few semi-colons

--- js/main.js
+++ js/main.js
@@ -9,8 +9,8 @@ let list = document.getElementById("mainList")
 links.forEach((items)=>{
     let li = document.createElement("li");
     let a = document.createElement("a");
-    a.href = links.url
-    a.textContent = links.label
-    li.appendChild(a)
-    list.appendChild(li)
+    a.href = links.url;
+    a.textContent = links.label;
+    li.appendChild(a);
+    list.appendChild(li);
 })
